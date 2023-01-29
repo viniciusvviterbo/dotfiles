3da85f369ddb0845191490e6246bc630e4cf8615
Vinícius Viterbo <v.viterbo@vp6.com.br>
2023-01-28 21:54:56 -0300

    dotfiles: folder structure refactored for stow use

---
+++ neovim/.config/nvim/lua/core/plugin_config/lsp_config.lua
@@ -0,0 +1,46 @@
+servers = {
+	"arduino_language_server",
+	"bashls",
+	"clangd",
+	-- "csharp_ls",
+	"clangd",
+	"cssls",
+	"dockerls",
+	"eslint",
+	-- "golangci_lint_ls",
+	"html",
+	"jsonls",
+	"jdtls",
+	"tsserver",
+	"ltex",
+	"sumneko_lua",
+	"marksman",
+	"jedi_language_server",
+	"rust_analyzer",
+	"sqlls",
+}
+
+require("mason").setup()
+require("mason-lspconfig").setup({
+	ensure_installed = servers,
+	automatic_installation = true
+})
+
+--local on_attach = function(_, bufnr)
+--    vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, {})
+--    vim.keymap.set('n', '<Leader>ca', vim.lsp.buf.code_action, {})
+--
+--    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+--    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+--    vim.keymap.set('n', 'gr', require('telescope.builtin'), {})
+--		vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+--end
+
+require("lspconfig").jedi_language_server.setup {
+	on_attach = on_attach
+}
+
+require("lspconfig").sumneko_lua.setup {
+    on_attach = on_attach
+}
+
---
+++ neovim/.config/nvim/lua/core/plugin_config/treesitter.lua
@@ -0,0 +1,40 @@
+require'nvim-treesitter.configs'.setup {
+    -- A list of parser names, or "all"
+    ensure_installed = {
+        "arduino",
+        "bash",
+        "c",
+        "c_sharp",
+        "cpp",
+        "css",
+        "diff",
+        "dockerfile",
+        "git_rebase",
+        "gitcommit",
+        "gitignore",
+        "go",
+        "graphql",
+        "html",
+        "java",
+        "javascript",
+        "json",
+        "latex",
+        "lua",
+        "make",
+        "markdown",
+        "python",
+        "regex",
+        "rust",
+        "scss",
+        "sql",
+        "typescript",
+        "vim",
+        "yaml" },
+
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+        enable = true,
+    },
+}

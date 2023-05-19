815c673421d25c64c36cba2e6e37b5de2ed6e7f7
Vinícius Viterbo <v.viterbo@vp6.com.br>
2023-05-19 15:55:35 -0300

    neovim: nvim-tree shows hidden dotfiles

--- neovim/.config/nvim/lua/core/plugin_config/nvim-tree.lua
+++ neovim/.config/nvim/lua/core/plugin_config/nvim-tree.lua
@@ -1,6 +1,10 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+	git = {
+		ignore = false,
+	}
+})
 
 vim.keymap.set('n', '<Leader>e', ':NvimTreeFindFileToggle<CR>')

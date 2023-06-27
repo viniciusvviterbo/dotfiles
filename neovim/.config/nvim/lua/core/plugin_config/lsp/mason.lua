78d63588b077c7475b415e4351081d0a000e8303
Vinícius Viterbo <v.viterbo@vp6.com.br>
2023-06-27 14:55:08 -0300

    neovim: LSP for golang installed

--- neovim/.config/nvim/lua/core/plugin_config/lsp/mason.lua
+++ neovim/.config/nvim/lua/core/plugin_config/lsp/mason.lua
@@ -8,6 +8,7 @@ local servers = {
 	"jsonls",
 	-- "yamlls",
 	"rust_analyzer",
+	"gopls"
 }
 
 local settings = {
--- neovim/.config/nvim/lua/core/plugin_config/lsp_config.lua
+++ neovim/.config/nvim/lua/core/plugin_config/lsp_config.lua
@@ -8,6 +8,7 @@ servers = {
 	"dockerls",
 	"eslint",
 	-- "golangci_lint_ls",
+	"gopls",
 	"html",
 	"jsonls",
 	"jdtls",
@@ -26,24 +27,26 @@ require("mason-lspconfig").setup({
 	automatic_installation = true
 })
 
---local on_attach = function(_, bufnr)
---    vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, {})
---    vim.keymap.set('n', '<Leader>ca', vim.lsp.buf.code_action, {})
---
---    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
---    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
---    vim.keymap.set('n', 'gr', require('telescope.builtin'), {})
---		vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
---end
+local on_attach = function(_, bufnr)
+	vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, {})
+	vim.keymap.set('n', '<Leader>ca', vim.lsp.buf.code_action, {})
+
+	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+	vim.keymap.set('n', 'gr', require('telescope.builtin'), {})
+	vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+end
 
 require("lspconfig").jedi_language_server.setup {
 	on_attach = on_attach
 }
 
 require("lspconfig").sumneko_lua.setup {
-    on_attach = on_attach
+	on_attach = on_attach
 }
 
 require("lspconfig").rust_analyzer.setup {
-    on_attach = on_attach
+	on_attach = on_attach
 }
+
+require("lspconfig").gopls.setup {}
